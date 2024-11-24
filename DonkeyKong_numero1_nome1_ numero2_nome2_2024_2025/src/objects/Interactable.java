c15e458f84b238ded7a471d0c12062be6dd7def8
Miguel Paiva <mike.almeida.paiva@gmail.com>
2024-11-24 01:51:08 +0000

    update4.5

--- DonkeyKong_numero1_nome1_ numero2_nome2_2024_2025/src/objects/Interactable.java
+++ DonkeyKong_numero1_nome1_ numero2_nome2_2024_2025/src/objects/Interactable.java
@@ -1,9 +1,6 @@
 package objects;
 
-import pt.iscte.poo.utils.Direction;
-import pt.iscte.poo.utils.Point2D;
-
 public interface Interactable {
 	
-	public void interact(GameElement ge, Point2D position, Direction d);
+	public void interact(Manel manel);
 }
