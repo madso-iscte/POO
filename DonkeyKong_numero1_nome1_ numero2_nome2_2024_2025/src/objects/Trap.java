5849ebb729560c3aea4a509cd06c6a5f8c0d59ee
madso <madso@iscte-iul.pt>
2024-12-05 10:21:38 +0000

    update14

--- DonkeyKong_numero1_nome1_ numero2_nome2_2024_2025/src/objects/Trap.java
+++ DonkeyKong_numero1_nome1_ numero2_nome2_2024_2025/src/objects/Trap.java
@@ -1,6 +1,8 @@
 package objects;
 
+import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.Point2D;
+import pt.iscte.poo.game.GameEngine;
 import pt.iscte.poo.gui.ImageTile;
 
 public class Trap extends GameElement implements ImageTile, Intransposable {
@@ -22,6 +24,21 @@ public class Trap extends GameElement implements ImageTile, Intransposable {
 	@Override
 	public boolean isTransposable() {
 		return false;
+	}
+	
+	public void checkCollisionWithManel(Manel manel) {
+	    Point2D positionBelowManel = manel.getPosition().plus(Direction.DOWN.asVector());
+	    if (this.getPosition().equals(positionBelowManel)) {
+	        manel.setVida(manel.getVida() - 10);
+	        GameEngine.getInstance().getGui().setStatusMessage("Manel was attacked! Life " + manel.getVida() + "/100");
+			System.out.println("Manel atingido pela armadilha! Vida restante: " + manel.getVida());
+			
+			if(manel.getVida()<=0) {
+				manel.semVida();
+				GameEngine.getInstance().getCurrentRoom().removeElementAt(manel.getPosition(),manel);
+				GameEngine.getInstance().getGui().removeImage(manel);
+			}
+	    }
 	}
 
 
